333e8a25fdd15c5a1dbe1bd5961d52f6903f2cd8
vaibhav <vaibhavpandey382@gmail.com>
2025-09-15 23:18:32 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
-    // This is the configuration for building a library
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'ContentstackChatSDK',
-      fileName: 'contentstack-chat-sdk'
+      fileName: 'contentstack-chat-sdk',
+      formats: ['es'],
     },
     rollupOptions: {
-      // Make sure to externalize deps that shouldn't be bundled
-      // into your library (React in this case)
+      // This is the key change:
+      // We are telling Vite to not bundle these packages.
+      // They will be provided by the host application.
       external: ['react', 'react-dom'],
       output: {
         globals: {
           react: 'React',
-          'react-dom': 'ReactDOM'
-        }
-      }
-    }
-  }
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
+
